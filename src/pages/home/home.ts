9e3b16e2a1bc80ca9018a9209aea5ca2b3d0d583
nemza4 <nemavholags@gmail.com>
2018-06-26 14:58:50 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,39 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+value =0;
+values =0; 
   constructor(public navCtrl: NavController) {
 
   }
+  onclickThree(){
+    this.value+=3;
+    console.log(this.value);
+  }
 
+  onclickTwo(){
+    this.value+=2;
+    console.log(this.value);
+  }
+  
+  onclickfFreeThrow(){
+    this.value+=1;
+    console.log(this.value);
+  }
+  onclickThrees(){
+    this.values+=3;
+    console.log(this.values);
+  }
+  onclickTwos(){
+    this.values+=2;
+    console.log(this.values);
+  }
+  onclickfFreeThrows(){
+    this.values+=1;
+    console.log(this.values);
+  }
+  Reset(){
+    this.value=0;
+    this.values=0;
+  }
 }
